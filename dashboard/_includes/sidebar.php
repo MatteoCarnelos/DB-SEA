4e3c321f3ab89269e63867c29193580e7f443d54
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-01-29 11:31:06 +0100

    Imported old repository

---
+++ dashboard/_includes/sidebar.php
@@ -0,0 +1,24 @@
+<nav class="col-2 bg-light sidebar">
+    <div class="sidebar-sticky d-flex flex-column">
+    <div>
+        <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link <?php if($active == 0) echo 'active' ?>" href="./">
+            <i data-feather="home"></i>Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php if($active == 1) echo 'active' ?>" href="reports.php">
+            <i data-feather="alert-triangle"></i>Segnalazioni</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php if($active == 2) echo 'active' ?>" href="medicines.php">
+            <i data-feather="book-open"></i>Farmaci</a>
+        </li>
+        </ul>
+    </div>
+    <div class="mx-auto mt-auto mb-1">
+        <img src="logo-unipd.png" style="width: 75px; height: 75px;" class="img-fluid" alt="Unipd Logo">
+    </div>
+    <p class="text-muted text-center"><small>Carnelos - Kumar - Provera</small></p>
+    </div>
+</nav>
---
+++ dashboard/reports.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Matteo Carnelos">
+    <title>Pannello di controllo</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="dashboard.css" rel="stylesheet">
+    <script src="https://unpkg.com/feather-icons"></script>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <?php include '_includes/navbar.html'; ?>
+    
+    <div class="container-fluid">
+      <!-- Sidebar -->
+      <div class="row">
+        <?php 
+          $active = 1;
+          include '_includes/sidebar.php';
+        ?>
+
+        <!-- Main area -->
+        <main role="main" class="col-10 ml-auto px-4">
+          <div class="d-flex justify-content-between align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Segnalazioni</h1>
+          </div>
+
+          <div class="card">
+            <h5 class="card-header">Nuova segnalazione</h5>
+            <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+          </div>
+          
+
+        </main>
+      </div>
+    </div>
+
+    <script> feather.replace() </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
