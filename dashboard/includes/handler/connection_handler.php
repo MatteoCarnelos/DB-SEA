93c1454dd47469c75b37b3ed2857e3498460d876
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-02-07 16:37:29 +0100

    Updated credentials

--- dashboard/includes/handler/connection_handler.php
+++ dashboard/includes/handler/connection_handler.php
@@ -6,5 +6,5 @@ $err_alert = '
   </div>
 ';
 
-$dbconn = pg_connect("host=localhost dbname=carnelosma user=carnelosma password=Mat.Uni2019")
+$dbconn = pg_connect("host=dbstud.dei.unipd.it dbname=carnelosma user=carnelosma password=Mat.Uni2019")
   or die($err_alert);
